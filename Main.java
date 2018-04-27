e7cf520e572bb2dec58e49054eb91e57d78e4ea3
hemalrahman <38746367+hemalrahman@users.noreply.github.com>
2018-04-28 05:44:14 +0600

    This is main class
    This class contains main project information

---
+++ Main.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	static Scanner scan=new Scanner(System.in);
+    public static void main(String[] args) {
+        Current current=new Current();
+        New neww=new New();
+        Formulary formulary=new Formulary();
+        int n;
+        System.out.println("\nEnter Your Choice : ");
+        System.out.println("\n1. Current Medication .");
+        System.out.println("\n2. New Medication .");
+        System.out.println("\n3. Formulary .\n");
+        n=scan.nextInt();
+        if (n==1)
+        {
+            current.checkDose();
+            current.printDose();
+        }
+        else if(n==2)
+        {
+        	System.out.println("\nFor New Medication  : ");
+            neww.druglist();
+        }
+        else if(n==2)
+        {
+        	System.out.println("\nFor Formulary  : ");
+        	formulary.druglist();
+        }
+    }
+    
+
+}
