4d74c6c2c3fc8169f36d8eada67f71bbfd0fc78d
Maksudur Rahman <30984219+rotnocse@users.noreply.github.com>
2018-04-27 20:31:07 +0600

    Update Formulary.java

--- Formulary.java
+++ Formulary.java
@@ -8,18 +8,7 @@ public class Formulary {
 	Scanner scan=new Scanner(System.in);
 	 ArrayList<String> drugList = new ArrayList<String>();
 	String checkdrug;
-	    public void druglist()
-	    {
-	      
-	        drugList.add("Duralux");
-	        drugList.add("histasin");
-	        drugList.add("Tusca");
-	        drugList.add("disprin");
-	         System.out.println("Please Enter the drug name to be search : ");
-	        checkdrug=scan.nextLine();
-		    
-		  
-	    }
+	    
 	    public void printDrugList()
 	    {
 	        System.out.println("This is new Druglist "+drugList);
