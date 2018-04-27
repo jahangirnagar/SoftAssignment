587a9176ac0d6fdab64eaa87c703bf9e357532b3
Maksudur Rahman <30984219+rotnocse@users.noreply.github.com>
2018-04-27 20:31:47 +0600

    Update Formulary.java

--- Formulary.java
+++ Formulary.java
@@ -5,14 +5,7 @@ import java.util.Scanner;
 public class Formulary {
 
 
-	Scanner scan=new Scanner(System.in);
-	 ArrayList<String> drugList = new ArrayList<String>();
-	String checkdrug;
-	    
-	    public void printDrugList()
-	    {
-	        System.out.println("This is new Druglist "+drugList);
-	    }
+	
 
 
 }
