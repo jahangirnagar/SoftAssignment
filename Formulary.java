e35d526a983bf6a2d21bf3f8c2e5260c4eac9fbe
Maksudur Rahman <30984219+rotnocse@users.noreply.github.com>
2018-04-27 19:10:54 +0600

    This is Formulary class
    Here check drug from the list and check drug in the list exist or not.

---
+++ Formulary.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+
+public class Formulary {
+
+
+	 ArrayList<String> drugList = new ArrayList<String>();
+	    public void druglist()
+	    {
+	      
+	        drugList.add("Seclo");
+	        drugList.add("Beklo");
+	        drugList.add("Napa");
+	        drugList.add("Perasitamol");
+	        System.out.println(drugList);
+	    }
+	    public void printDrugList()
+	    {
+	        System.out.println("This ios new Druglist "+drugList);
+	    }
+
+}
